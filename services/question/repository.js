0ee4a241ea7b2c8938599b19805eb0fc9dcad9d2
Caick Passarella <caickpassarella@hotmail.com>
2020-01-30 11:37:20 -0300

    Added deleteAll and improved mongo connect

--- services/question/repository.js
+++ services/question/repository.js
@@ -1,7 +1,6 @@
 const questionModel = require("../../models/questionModel");
 
 async function create(question) {
-    console.log(question);
     return await questionModel.create(question);
 }
 
@@ -9,17 +8,22 @@ async function getAll() {
     return await questionModel.find();
 }
 
-async function getOne(filter) {
-    return await questionModel.findOne(filter);
+async function getOne(_id) {
+    return await questionModel.findById(_id);
 }
 
 async function deleteOne(category) {
-    return await questionModel.findOneAndDelete(category)
+    return await questionModel.findOneAndDelete(category);
+}
+
+async function deleteAll() {
+    return await questionModel.deleteMany();
 }
 
 module.exports = {
     create,
     getAll,
     getOne,
-    deleteOne
+    deleteOne,
+    deleteAll
 };
